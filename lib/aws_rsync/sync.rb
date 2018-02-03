6eea7744be78cc4c43e3a5c6060752500525228a
Tung Nguyen <tongueroo@gmail.com>
2018-02-02 16:46:48 -0800

    add AWS_RSYNC_EXCLUDE AWS_RSYNC_OPTIONS override ability

--- lib/aws_rsync/sync.rb
+++ lib/aws_rsync/sync.rb
@@ -16,6 +16,20 @@ module AwsRsync
     end
 
     def command
+      main_options = build_main_options
+      exclude = build_exclude_options
+      options = "#{main_options} #{exclude}"
+      dest = "#{user}@#{host.ip}:#{folder}"
+
+      "rsync #{options} #{src} #{dest}"
+    end
+
+    # A default set of options is provided.
+    # You can override this with the environment variable AWS_RSYNC_OPTIONS.
+    #
+    def build_main_options
+      return ENV['AWS_RSYNC_OPTIONS'] if ENV['AWS_RSYNC_OPTIONS']
+
       # --numeric-ids               don't map uid/gid values by user/group name
       # --safe-links                ignore symlinks that point outside the tree
       # -a, --archive               recursion and preserve almost everything (-rlptgoD)
@@ -23,20 +37,23 @@ module AwsRsync
       # -z, --compress              compress file data during the transfer
       # -S, --sparse                handle sparse files efficiently
       # -v, --verbose               verbose
-      exclude = %w/.git tmp log/
+      #     --delete                delete extraneous files from destination dirs
+      #
+      "--delete --numeric-ids --safe-links -axzSv"
+    end
+
+    # The rsync exclude options are automatically calculated and added with some
+    # sane defaults. You can override the options with the environment variable
+    # AWS_RSYNC_EXCLUDE.  To have no exclude options at all use a blank string:
+    # AWS_RSYNC_EXCLUDE=''
+    def build_exclude_options
+      return ENV['AWS_RSYNC_EXCLUDE'] if ENV['AWS_RSYNC_EXCLUDE']
+
+      exclude = %w/.git tmp log/ # default values
       exclude += get_excludes('.gitignore')
       exclude += get_excludes('.dockerignore')
       exclude = exclude.uniq.map{|path| "--exclude='#{path}'"}.join(' ')
-      options = "--delete --numeric-ids --safe-links -axzSv #{exclude}"
-      src = get_src
-      dest = "#{user}@#{host.ip}:#{folder}"
-
-      "rsync #{options} #{src} #{dest}"
-    end
-
-    def get_src
-      src = @options[:cwd] || Dir.pwd
-      src[-1] == '/' ? src : "#{src}/" # ensure trailing /
+      exclude
     end
 
     def user
@@ -49,9 +66,13 @@ module AwsRsync
 
     # destination folder
     def folder
-      src = get_src
       src = src == '.' ? Dir.pwd : src
       @options[:folder] || File.basename(src)
+    end
+
+    def src
+      src = @options[:cwd] || Dir.pwd
+      src[-1] == '/' ? src : "#{src}/" # ensure trailing /
     end
 
     def get_excludes(file)
